58ef5982a9874e5ad6ce3fb65069e5a31763db8f
MARlon024 <marlondurand675@gmail.com>
2022-11-11 09:12:55 -0500

    Correcion

--- SB_2.0/persona.py
+++ SB_2.0/persona.py
@@ -1,7 +1,7 @@
 class Persona():
     def __init__(self ,__nom, __dni,__ape_pa,__ape_ma,__correo,__fecha,__cel=None,__cel_ope=None):
         self.nom = __nom
-        self.__dni = __dni
+        self.dni = __dni
         self.ape_pa=__ape_pa
         self.ape_ma=__ape_ma
         self.correo=__correo
